79eb0d11efd3e2d617da448b6e48a7c3c20abd35
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-03-08 21:18:08 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Dropdown } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [selectedSet, setSelectedSet] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <h3>Mandarin Flashcards</h3>
+        <div className="small-text">Test your knowledge.</div>
+        <br />
+        <Dropdown
+          className="dropdown-container"
+          onSelect={(e) => {
+            console.log(e);
+          }}
         >
-          Learn React
-        </a>
+          <Dropdown.Toggle>
+            {selectedSet || "Select flashcards"}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="test">test</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </header>
+      <main>main</main>
+      <footer>footer</footer>
     </div>
   );
 }
