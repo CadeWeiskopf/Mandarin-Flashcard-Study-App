278f6b619979c6b606640580755f1f4a91c1d71d
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-03-09 23:09:26 -0500

    fliscard setup and naming fix

---
+++ src/components/FlashcardCard.tsx
@@ -0,0 +1,9 @@
+import { Flashcard } from "../interfaces/Flashcard";
+
+export default function FlashcardCard(props: { flashcard: Flashcard }) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
--- src/components/FlashcardDeck.tsx
+++ src/components/FlashcardDeck.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { Flashcard } from "../interfaces/Flashcard";
+import FlashcardCard from "./FlashcardCard";
 
 export default function FlashcardDeck(props: { flashcards: Flashcard[] }) {
+  const [currentCard, setCurrentCard] = useState(-1);
   return (
     <>
-      {props.flashcards.map((flashcard) => {
-        return <>{JSON.stringify(flashcard)}</>;
-      })}
+      <div className="flashcarddeck">
+        {currentCard < 0 ? (
+          <button
+            type="button"
+            onClick={() => {
+              setCurrentCard(
+                Math.floor(Math.random() * props.flashcards.length)
+              );
+            }}
+          >
+            start
+          </button>
+        ) : (
+          <FlashcardCard flashcard={props.flashcards[currentCard]} />
+        )}
+      </div>
     </>
   );
 }
